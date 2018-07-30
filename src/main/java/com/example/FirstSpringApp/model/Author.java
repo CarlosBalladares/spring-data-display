b4f03573fa9aef4cd19cecdb70526353075398cf
Carlos Balladares <carlos@balladares.co>
2018-07-30 08:13:26 -0700

    please work

--- src/main/java/com/example/FirstSpringApp/model/Author.java
+++ src/main/java/com/example/FirstSpringApp/model/Author.java
@@ -25,7 +25,7 @@ import javax.persistence.ManyToMany;
 
 public class Author  implements Serializable {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String firstName;
   private String lastName;
