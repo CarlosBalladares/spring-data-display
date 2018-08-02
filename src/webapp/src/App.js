24fb5a3d99a393d3a1e885ea2fbbe7d6de99a53e
Carlos Balladares <carlos@balladares.co>
2018-08-01 23:24:02 -0700

    adding app js file

--- src/webapp/src/App.js
+++ src/webapp/src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 import Header from './components/Header';
 import Books from './components/Books';
 import Authors from './components/Authors';
+import {BrowserRouter as Router, Route} from 'react-router';
 
 
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App" style={{'display':'flex','justify-content':'center', 'flex-direction':'column',}}>
         <Header/>
         <Books/>
-        <Authors/>
       </div>
+      </Router>
     );
   }
 }
